5d7fbe4e2d0844315c23791cf3a60780e2650425
Case Kramer <>
2019-10-11 10:33:46 -0400

    App cleanup

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,7 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyDqI6CEQwlxHgi5VAL-vcUy28AzTA7jtfk",
-    authDomain: "life-manager-3906b.firebaseapp.com",
-    databaseURL: "https://life-manager-3906b.firebaseio.com",
-    projectId: "life-manager-3906b",
-    storageBucket: "",
-    messagingSenderId: "234735139122",
-    appId: "1:234735139122:web:5e7e6af8e7cc5924643f2b",
-    measurementId: "G-CP7WW72P9Z"
+    //Add firebase config here
 };
 
 firebase.initializeApp(config);
